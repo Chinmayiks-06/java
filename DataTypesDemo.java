665c6b20eb12f7b40c3cb37d2922fe708acf6585
Chinmayiks-06 <chinmayiks633@gmail.com>
2025-08-28 18:53:46 +0530

    Add files via upload

---
+++ DataTypesDemo.java
@@ -0,0 +1,15 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int age = 20;
+        double salary = 35000.50;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Chinmayt";
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+        System.out.println("Name: " + name);
+    }
+}
---
+++ LargestOfTwo.java
@@ -0,0 +1,11 @@
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        int a = 25, b = 30;
+
+        if (a > b) {
+            System.out.println(a + " is larger");
+        } else {
+            System.out.println(b + " is larger");
+        }
+    }
+}
